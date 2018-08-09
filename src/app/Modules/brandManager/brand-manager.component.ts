073feb76df359ccf78461165cbc5eb8160d9f81d
Nyaz Khan <khannyaz1705@gmail.com>
2018-08-09 12:44:09 +0530

    add manager form

--- src/app/Modules/brandManager/brand-manager.component.ts
+++ src/app/Modules/brandManager/brand-manager.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '../../../../node_modules/@angular/router';
 import { BrandManagerService } from './brand-manager.service';
+import { ManagerDetails } from '../../interface/manager_details';
 
 @Component({
   selector: 'app-brand-manager',
@@ -13,6 +14,7 @@ export class BrandManagerComponent implements OnInit {
 
   dataRows: any;
   isDataLoad: boolean = false;
+  managerDetails= new ManagerDetails;
 
   headerRow: Array<string> = [
     'S.No.',
@@ -21,8 +23,15 @@ export class BrandManagerComponent implements OnInit {
     'User Name',
     'Phone No',
     'Email',
+   
   ]
   ngOnInit() {
+    this.getManagers();
+   this.getBrandIds();
+  }
+
+
+  getManagers(){
     this.brandService.getManager()
     .subscribe((res:any)=>{
      this.dataRows= res;
@@ -35,12 +44,53 @@ export class BrandManagerComponent implements OnInit {
             })
   }
 
-
-  goto() {
-    this.router.navigate(["/manager/addManager"])
+  
+  getBrandIds(){
+  this.brandService.getBrandIds()
+      .subscribe((res:any)=>{
+       this.brands=res;
+       console.log(res)
+       this.isBrandId=false;
+        // this.pins=res;
+        // this.pinIstrue= false;
+      },
+    (err)=>{
+      alert(JSON.stringify(err));
+    })
   }
 
 
 
+  loadingButton:boolean=false;
+  isBrandId:boolean=false;
+  brands:any;
+    onSubmit(){
+      this.loadingButton=true;
+      const fd =  new FormData();
+  
+  for(const key in this.managerDetails){
+    fd.append(key, this.managerDetails[key]);
+    }
+  
+  this.brandService.addManager(fd)
+  .subscribe((res:any) => {
+    // store token 
+    // localStorage.setItem("access_token",res.access_token)
+            // this.router.navigate(['/main/manager']);
+            this.resetform();
+          }, (err) => {
+            
+            alert(JSON.stringify(err));
+                // throw err;  
+          })
+   
+  
+  }
+  
 
+
+  resetform(){
+
+        this.managerDetails= new ManagerDetails();
+      }
 }
